3f8f42a896e19189c191d821836440d82172e59c
didier <didier.giovanetti@gmail.com>
2017-11-20 21:36:49 +0100

    tech: eslint tests: add globals & rules for sinon

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -3,8 +3,14 @@ module.exports = {
     "describe": false,
     "it": false,
     "beforeAll": false,
+    "beforeEach": false,
+    "afterAll": false,
+    "afterEach": false,
     "expect": false,
     "shallow": false,
     "browser": false
+  },
+  "rules": {
+    "no-unused-expressions": "off"
   }
 };
