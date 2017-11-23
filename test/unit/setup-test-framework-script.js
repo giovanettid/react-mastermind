196e1c8cf56922b3faf4419e0cb97580a17f54ae
didier <didier.giovanetti@gmail.com>
2017-11-24 00:58:19 +0100

    tech: tests setup, add global mount

--- test/unit/setup-test-framework-script.js
+++ test/unit/setup-test-framework-script.js
@@ -1,11 +1,12 @@
 import 'raf/polyfill';
 
-import { configure, shallow } from 'enzyme';
+import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import { expect } from 'chai';
 
 global.expect = expect;
 global.shallow = shallow;
+global.mount = mount;
 
 configure({ adapter: new Adapter() });
