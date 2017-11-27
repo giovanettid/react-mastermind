a7d41e50241b702da9f63451b2fa1211a3db80da
didier <didier.giovanetti@gmail.com>
2017-11-27 21:49:09 +0100

    feature: add colors prop to ColorPicker

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import ColorItem from 'components/ColorItem/ColorItem';
 
-export default function ColorPicker() {
-  const items = [...Array(6).keys()].map(e => <ColorItem key={e} />);
+const ColorPicker = (props) => {
+  const items = props.colors
+    .map(color => <ColorItem key={color} color={color} />);
+
   return (
     <div className="colorPicker">{items}</div>
   );
-}
+};
+
+ColorPicker.propTypes = {
+  colors: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default ColorPicker;
--- test/unit/components/ColorPicker.test.jsx
+++ test/unit/components/ColorPicker.test.jsx
@@ -3,9 +3,13 @@ import React from 'react';
 import ColorPicker from 'components/ColorPicker/ColorPicker';
 
 describe('ColorPicker', () => {
-  it('should display ColorPicker with 6 ColorItem', () => {
-    const wrapper = mount(<ColorPicker />);
+  it('should display ColorPicker with n ColorItem', () => {
+    const wrapper = mount(<ColorPicker
+      colors={['Yellow', 'Black']}
+    />);
 
-    expect(wrapper.find('.colorItem')).to.have.lengthOf(6);
+    expect(wrapper.find('.colorItem')).to.have.lengthOf(2);
+    expect(wrapper.find('.colorYellow').exists()).to.be.true;
+    expect(wrapper.find('.colorBlack').exists()).to.be.true;
   });
 });
