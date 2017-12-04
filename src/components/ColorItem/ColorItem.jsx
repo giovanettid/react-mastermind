4c6f799b60c3612be568170336e66a78f28b40e7
didier <didier.giovanetti@gmail.com>
2017-12-04 20:51:42 +0100

    feature: style, add colors class

--- src/components/ColorItem/ColorItem.jsx
+++ src/components/ColorItem/ColorItem.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './ColorItem.scss';
+
 const ColorItem = (props) => {
   const className = `colorItem color${props.color}`;
   return <td className={className} />;
