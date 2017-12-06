f8afc778d629ab16a126404930f91db10f8f3de5
didier <didier.giovanetti@gmail.com>
2017-12-06 23:10:52 +0100

    feat: modify template in a row

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -4,12 +4,29 @@ import CodeHole from 'components/CodeHole/CodeHole';
 import KeyHole from 'components/KeyHole/KeyHole';
 
 export default function Row() {
-  const NB_HOLES = 4;
-  const holeIterator = [...Array(NB_HOLES).keys()];
-  const codeHoles = holeIterator.map(e => <CodeHole key={e} />);
-  const keyHoles = holeIterator.map(e => <KeyHole key={e} />);
+  const NB_CODE_HOLES = 4;
+  const codeHolesRow = [...Array(NB_CODE_HOLES).keys()].map(e => <CodeHole key={e} />);
+
+  const NB_KEY_HOLES = 2;
+  const keyHolesRow = [...Array(NB_KEY_HOLES).keys()].map(e => <KeyHole key={e} />);
 
   return (
-    <tr className="Row">{codeHoles}{keyHoles}</tr>
+    <tr className="Row">
+      <td>
+        <table>
+          <tbody>
+            <tr>{codeHolesRow}</tr>
+          </tbody>
+        </table>
+      </td>
+      <td>
+        <table>
+          <tbody>
+            <tr>{keyHolesRow}</tr>
+            <tr>{keyHolesRow}</tr>
+          </tbody>
+        </table>
+      </td>
+    </tr>
   );
 }
