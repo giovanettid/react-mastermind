8884f1a48bf2da1d3d16d0592ac5867be38169a3
didier <didier.giovanetti@gmail.com>
2017-12-08 15:40:48 +0100

    feat: Rows receive nextCode prop

--- src/components/Rows/Rows.jsx
+++ src/components/Rows/Rows.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Row from 'components/Row/Row';
 
-const Rows = () => {
+// eslint-disable-next-line no-unused-vars
+const Rows = ({ nextCode }) => {
   const NB_ROWS = 10;
   const rows = [...Array(NB_ROWS).keys()].map(e => <Row key={e} />);
 
@@ -11,6 +13,13 @@ const Rows = () => {
       <tbody className="Rows">{rows}</tbody>
     </table>
   );
+};
+
+Rows.propTypes = {
+  nextCode: PropTypes.shape({
+    row: PropTypes.number,
+    item: PropTypes.number,
+  }).isRequired,
 };
 
 export default Rows;
--- test/unit/components/Rows.test.jsx
+++ test/unit/components/Rows.test.jsx
@@ -3,9 +3,21 @@ import React from 'react';
 import Rows from 'components/Rows/Rows';
 
 describe('Rows', () => {
-  it('should display Rows with 10 Row', () => {
-    const wrapper = mount(<Rows />);
+  let wrapper;
 
-    expect(wrapper.find('.Row')).to.have.lengthOf(10);
+  beforeEach(() => {
+    wrapper = mount(<Rows nextCode={{ row: 2, item: 1 }} />);
+  });
+
+  describe('render', () => {
+    it('should display Rows with 10 Row', () => {
+      expect(wrapper.find('.Row')).to.have.lengthOf(10);
+    });
+  });
+
+  describe('props', () => {
+    it('should receive nextCode prop', () => {
+      expect(wrapper.prop('nextCode')).to.deep.equal({ row: 2, item: 1 });
+    });
   });
 });
