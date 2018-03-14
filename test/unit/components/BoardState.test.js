22848ffba16c44a7342044c76c6fbd829353a8eb
didier <didier.giovanetti@gmail.com>
2018-03-14 21:17:43 +0100

    tech: BoardState, add unit tests

--- test/unit/components/BoardState.test.js
+++ test/unit/components/BoardState.test.js
@@ -3,6 +3,28 @@ import BoardState from 'components/Board/BoardState';
 describe('BoardState', () => {
   const state = new BoardState(2);
 
+  describe('isNextRow', () => {
+    it('should return true when set to last item', () => {
+      expect(state.isNextRow({ row: 1, item: 2 })).to.be.true;
+    });
+
+    it('should return false when set to first item', () => {
+      expect(state.isNextRow({ row: 1, item: 1 })).to.be.false;
+    });
+  });
+
+  describe('nextRow', () => {
+    it('should return move state to next row, first item', () => {
+      expect(BoardState.nextRow({ row: 1, item: 2 })).to.deep.equal({ row: 2, item: 1 });
+    });
+  });
+
+  describe('nextItem', () => {
+    it('should return move state to next item, same row', () => {
+      expect(BoardState.nextItem({ row: 1, item: 1 })).to.deep.equal({ row: 1, item: 2 });
+    });
+  });
+
   describe('getNext', () => {
     it('should move from initial position to first item', () => {
       const prev = { row: 0, item: 0, boardColors: [['lightgrey', 'lightgrey'], ['lightgrey', 'lightgrey']] };
